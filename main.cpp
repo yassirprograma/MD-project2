811aa3829f21c113d052a977c1e69c8a866cf7fd
yassirprograma <dj.jicara@gmail.com>
2020-04-17 16:08:33 -0500

    Agregamos producto cartesiano y conjunto potencia

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 using namespace std;
 
-struct conjunto { //estructura con las caracteristicas de cada conjunto
+struct conjunto{ //estructura con las caracteristicas de cada conjunto
         int tam; //cardinalidad de numeros aleatorios
         int bucket[12]; //este arreglo nos permite llevar a cabo el ordenamiento por bucketsort
         int conjuntoconrepetidos[102]; //guardara todos los numeros aleatorios generados
@@ -70,9 +71,9 @@ void difsim(){
 }
 
 void compA(){
-	int universe = 0;
+int universe = 0;
     for(int i=0;i<10;i++){
-    	universe++;
+    universe++;
         if(cA.bucket[i]>=1 != universe>=1){ //recorre la cubeta de ambos arreglos viendo si en ambos está el valor
             compa[poscompa]=i; //guarda los valores en el conjunto de interseccion
             poscompa++;
@@ -97,9 +98,9 @@ void compA(){
 }
 
 void compB(){
-	int universe = 0;
+int universe = 0;
     for(int i=0;i<10;i++){
-    	universe++;
+    universe++;
         if(cB.bucket[i]>=1 != universe>=1){ //recorre la cubeta de ambos arreglos viendo si en ambos está el valor
             compb[poscompb]=i; //guarda los valores en el conjunto de interseccion
             poscompb++;
@@ -219,12 +220,49 @@ void uni(){
     }
 
 }
+
+void conjuntopotenciaA(){
+    int cantidad=pow(2,(cA.posabsoluto));
+    for(int i=0;i<cantidad;i++)
+    {
+
+        cout<<"{";
+        for(int j=0;j<cA.tam;j++){
+          if(i & (1<<j)) cout<<cA.conjuntoabsoluto[j+1]<<" ";
+        }
+        cout<<"} "; if(i+1<cantidad) cout<<", ";
+    }
+}
+void conjuntopotenciaB(){
+    int cantidad=pow(2,(cB.posabsoluto));
+    for(int i=0;i<cantidad;i++)
+    {
+
+        cout<<"{";
+        for(int j=0;j<cB.tam;j++){
+            if(i & (1<<j))
+                cout<<cB.conjuntoabsoluto[j+1]<<" ";
+        }
+        cout<<"} "; if(i+1<cantidad) cout<<", ";
+
+    }
+
+}
+void productocartesianoAB(){
+    for(int i=0;i<cA.posabsoluto;i++){
+        for(int j=0;j<cB.posabsoluto;j++){
+            cout<<"("<<cA.conjuntoabsoluto[i+1]<<","<<cB.conjuntoabsoluto[j+1]<<"),";
+        }
+    }
+}
 int main()
 {
     puts("Escribe cuantos elementos aleatorios para el conjunto A");
     cin>>cA.tam;
     puts("Escribe cuantos elementos aleatorios para el conjunto B");
     cin>>cB.tam;
+
+    system("cls");
 
     srand (time(NULL)); //inicializa seed
     rango=rand()%10+1; //genera un rango aleatorio entre 1 y 10
@@ -242,15 +280,19 @@ int main()
                 cA.posabsoluto++;
             }
     }
-
+    cA.posabsoluto--;
     cout<<"El conjunto A aleatorio es: {";
     imprime(cA.conjuntoconrepetidos,cA.tam); cout<<"}"<<endl;  //imprime el conjunto original
 
     cout<<"El conjunto A sin repetidos es: {";
-    imprime(cA.conjuntoabsoluto,cA.posabsoluto-1); cout<<"}"<<endl; //imprime el conjunto sin repetir elementos
+    imprime(cA.conjuntoabsoluto,cA.posabsoluto); cout<<"}"<<endl; //imprime el conjunto sin repetir elementos
 
-    cout<<"Cuya cardinalidad es:"<<cA.posabsoluto-1<<endl; //imprimer la cardinalidad
-    cout<<endl;
+    cout<<"Cuya cardinalidad es:"<<cA.posabsoluto<<endl; //imprimer la cardinalidad
+
+    //conjunto potencia A
+    cout<<"El conjunto Potencia de A es: P(A){ ";
+    conjuntopotenciaA();
+    cout<<"}\n\n";
 
     //Conjunto B
     rango=rand()%10+1; //genera un rango aleatorio entre 1 y 10
@@ -267,40 +309,41 @@ int main()
                 cB.posabsoluto++;
             }
     }
-
+    cB.posabsoluto--;
     cout<<"El conjunto B aleatorio es: {";
     imprime(cB.conjuntoconrepetidos,cB.tam); cout<<"}"<<endl;  //imprime el conjunto original
 
     cout<<"El conjunto B sin repetidos es: {";
-    imprime(cB.conjuntoabsoluto,cB.posabsoluto-1); cout<<"}"<<endl; //imprime el conjunto sin repetir elementos
+    imprime(cB.conjuntoabsoluto,cB.posabsoluto); cout<<"}"<<endl; //imprime el conjunto sin repetir elementos
 
-    cout<<"Cuya cardinalidad es:"<<cB.posabsoluto-1<<endl<<endl; //imprimer la cardinalidad
-/*
-    ERICK, SI TE CONFUNDES DESCOMENTA ESTE PEDAZO DE CODIGO Y PUEDES VER COMO ESTÁ LA CUBETA DE CADA UNO
-    printf(" LA CUBETA DE A ES :\n");
-    for(int i=0;i<=10;i++){
-       cout<< cA.bucket[i]<<" ";
-    }
-    cout<<endl;
-    printf(" LA CUBETA DE B ES :\n");
-    for(int i=0;i<=10;i++){
-        cout<<cB.bucket[i]<<" ";
-    }
-    cout<<endl;
-*/
-    uni();
-    cout<<endl;
-    intersec();
-    cout<<endl;
-    compA();
-    cout<<endl;
-    compB();
-    cout<<endl;
-    diferab();
-    cout<<endl;
-    diferba();
-    cout<<endl;
-    difsim();
+    cout<<"Cuya cardinalidad es:"<<cB.posabsoluto<<endl; //imprimer la cardinalidad
+
+    //conjunto Potencia B
+    cout<<"El conjunto Potencia de B es: P(B){ ";
+    conjuntopotenciaB();
+    cout<<"}\n\n";
+    cout<<endl<<endl;
+    cout<<"El producto cartesiano de A y B es: { ";
+    productocartesianoAB();
+    cout<<"}\n\n";
+
+
+        system("pause");
+        system("cls");
+
+        uni();
+        cout<<endl;
+        intersec();
+        cout<<endl;
+        compA();
+        cout<<endl;
+        compB();
+        cout<<endl;
+        diferab();
+        cout<<endl;
+        diferba();
+        cout<<endl;
+        difsim();
 
 
     return 0;
